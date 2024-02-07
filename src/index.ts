46bb99c3cd332ccbfd365453312e015eed7226f7
Anton Halauko <anton.halauko.main@gmail.com>
2024-02-07 09:52:03 +0200

    init: install tools for development

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import * as http from 'node:http';
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify({ data: 'hello-world' }));
+});
+
+server.listen(8000);
